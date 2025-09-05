bf9b22442612fd767a586cfceed8da35334e987b
Ayush Patel <ayushdheerajpatel@gmail.com>
2025-09-05 20:33:15 +0530

    fix home route

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ main().then(() => {
 async function main() {
   await mongoose.connect(dbUrl);
 }
-
+// Home page route
+app.get("/", (req, res) => {
+  res.render("home");
+});
 // --- Middleware ---
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
